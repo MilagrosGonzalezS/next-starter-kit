b372260ffaf564071cc2a8a754dbeadb8d73d81a
Milagros Gonzalez <101905632+MilagrosGonzalezS@users.noreply.github.com>
2024-09-16 19:43:04 -0300

    our initial commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className="p-24">
+      <div className="container">
+        <h1 className="mt-3 text-3xl font-bold">NextJs 14 Starter Kit</h1>
+      </div>
+    </section>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
+export const content = [
+  './pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './components/**/*.{js,ts,jsx,tsx,mdx}',
+  './app/**/*.{js,ts,jsx,tsx,mdx}'
+]
+export const theme = {
+  container: {
+    center: true,
+    padding: {
+      DEFAULT: '1rem',
+      md: '1.5 rem',
+      lg: '2rem'
+    }
+  }
+}
+export const plugins = []
